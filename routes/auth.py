7538a31dfaf56767df6e9f166cef5321cfa5ff6a
McisnerosP <mabel.cisneros@ipt.pe>
2022-09-20 13:57:27 -0500

    aps

--- routes/auth.py
+++ routes/auth.py
@@ -9,12 +9,12 @@ from functions_jwt import write_token,validate_token
 auth_routes =APIRouter()
 class User(BaseModel):
     username : str
-    email: EmailStr
+    password: str
 
 @auth_routes.post("/login")
 def login(user: User):
     print(user.dict())
-    if user.username== 'Mabeley' and user.email== 'mabel.cisneros@ipt.pe':
+    if user.username== 'Mabeley' and user.password== '1234':
         return write_token(user.dict())
     else:
         return JSONResponse(content={"message": "User not found"}, status_code=404)
