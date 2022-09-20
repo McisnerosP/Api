5e57f38078aa0ac084ae098c422cf3e792371449
McisnerosP <mabel.cisneros@ipt.pe>
2022-09-20 10:52:35 -0500

    ja

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 from routes.auth import auth_routes
 from routes.user_api import info_api
-app= FastAPI()
+app= FastAPI("Autho")
 app.include_router(auth_routes)
 app.include_router(info_api)
 load_dotenv()
