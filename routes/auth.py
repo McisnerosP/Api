af3c37f810f99dd2ee68de95898d3de52f9ab4ee
McisnerosP <mabel.cisneros@ipt.pe>
2022-09-21 11:05:22 -0500

    getenv

--- routes/auth.py
+++ routes/auth.py
@@ -4,24 +4,27 @@ from os import getenv
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, EmailStr
 from functions_jwt import write_token,validate_token
+from dotenv import load_dotenv
 
-
+#load_dotenv()
 auth_routes =APIRouter()
 class User(BaseModel):
     username : str
     password: str
 
 @auth_routes.post("/login")
-def login(user: User):
+async def login(user: User):
     print(user.dict())
-    if user.username== 'Mabeley' and user.password== '1234':
+    if user.username == 'iptuser' and user.password == 'useriptpasword45':
         return write_token(user.dict())
     else:
         return JSONResponse(content={"message": "User not found"}, status_code=404)
-    
-@auth_routes.post("/verify/token")
-def verify_token(Authorization: str = Header(None)):
-    print(Authorization)
 
-    token = Authorization.split(" ")[1]
-    return validate_token(token, output=True)
+
+    
+# @auth_routes.post("/verify/token")
+# def verify_token(Authorization: str = Header(None)):
+#     print(Authorization)
+
+#     token = Authorization.split(" ")[1]
+#     return validate_token(token, output=True)
