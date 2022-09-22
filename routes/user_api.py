745f156c2759b5fb2821db7e20b7a81dbad9b20a
McisnerosP <mabel.cisneros@ipt.pe>
2022-09-22 17:13:37 -0500

    last changes

--- routes/user_api.py
+++ routes/user_api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from requests import get
+import requests
 from middlewares.verify_token_route import VerifyTokenRoute
 from os import getenv
 from dotenv import load_dotenv
@@ -11,7 +11,18 @@ load_dotenv()
 @info_api.get("/inventory")
 def api_inventory():
     # return get('https://inventory-restapi-h36rii5v2q-ue.a.run.app/inventariolist').json()
-    return get(f"{getenv('LINK_INVENTARIO')}").json()
+    return requests.get(f"{getenv('LINK_INVENTARIO')}").json()
+
+class Dato(BaseModel):
+    valor:str
+
+
+@info_api.get("/site/{valor}")
+def api_site(valor):
+    print(requests.post('https://inventory-restapi-h36rii5v2q-ue.a.run.app/infopersite/',json={"name":valor}))
+    return requests.post('https://inventory-restapi-h36rii5v2q-ue.a.run.app/infopersite/',json={"name":valor}).text
+   
+    
 
 
 
@@ -19,12 +30,19 @@ def api_inventory():
 
 
 
-# class Dato(BaseModel):
-#     valor:str
 
-# # @info_api.post("/site")
-# # def api_site(varibale:Sites):
-# #     return get(f'https://inventory-restapi-h36rii5v2q-ue.a.run.app/infopersite/"{varibale.name}"').json()
+
+
+
+
+
+
+
+
+
+
+
+
 # @info_api.post("/site/")
 # async def api_site(val:Dato):
 #     return get(f'https://inventory-restapi-h36rii5v2q-ue.a.run.app/infopersite/"{val.valor}"').json()
